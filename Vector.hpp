b8bff71fdbda4238e96cf17fa15c617b0d551fc3
Ayoub Maatouch <48140672+AyoubMaatouch@users.noreply.github.com>
2022-03-02 00:13:08 +0100

    adding new overloads

--- Vector.hpp
+++ Vector.hpp
@@ -1,9 +1,11 @@
 #pragma once
+#include <memory>
 
 namespace ft {
 
- class vector
-    {
+ template < class T, class Alloc = std::allocator<T> > class vector // generic template
+{
+            typedef Alloc allocator_type;
             //  Contracture
             explicit vector (const allocator_type& alloc = allocator_type());
             explicit vector (size_type n, const value_type& val = value_type(),
@@ -14,5 +16,6 @@ namespace ft {
                  const allocator_type& alloc = allocator_type());	
             //  Copy Contracture
             vector (const vector& x);
-    }
-}
+            private:
+                allocator_type _allocator;      
+};
--- iterators.hpp
+++ iterators.hpp
@@ -23,71 +23,127 @@ namespace ft
 {
 
     // i think this not as important
-    // template <class Category, class T, class Distance = size_t, // ptrdiff_t is sigend and size_t not
-    //   class Pointer = T *, class Reference = T &>
-    // struct iterBase
-    // {
-    // typedef T value_type;
-    // typedef Distance difference_type;
-    // typedef Pointer pointer;
-    // typedef Reference reference;
-    // typedef Category iterator_category;
-    // };
+    template <class Category, class T, class Distance = ptrdiff_t, // ptrdiff_t is sigend and size_t not
+      class Pointer = T *, class Reference = T &>
+    struct iterBase
+    {
+            typedef T value_type;
+            typedef Distance difference_type;
+            typedef Pointer pointer;
+            typedef Reference reference;
+            typedef Category iterator_category;
+    };
 
     template <class T>
-    class _Iterator //: public iterBase<std::random_access_iterator_tag, T>
+    class _Iterator : public iterBase<std::random_access_iterator_tag, T>
     {
 
     public:
-        typedef T value_type;
-        typedef size_t difference_type;
-        typedef T *pointer;
-        typedef T &reference;
-        typedef std::random_access_iterator_tag iterator_category;
+        typedef T                               value_type;
+        typedef ptrdiff_t                          difference_type; //ptrdiff_t is sigend and size_t not
+        typedef T*                              pointer;
+        typedef T&                              reference;
+        typedef Category iterator_category;
 
-        _Iterator(pointer p = NULL)
+        _Iterator(): _ptrBaseBase() {}
+        
+        _Iterator(pointer p)
         {
             std::cout << "iterator destractor called" << std::endl;
-            _ptr = p;
         }
+            _ptrBase = p;
         ~_Iterator() { std::cout << "iterator destractor called" << std::endl; }
 
         _Iterator(_Iterator const &src)
         {
-            _ptr = src._ptr;
+            _ptrBase = src._ptrBase;
             return;
         }
         _Iterator(_Iterator const &src)
         {
-            _ptr = src._ptr;
-            return;
+            _ptrBase = src._ptrBase;
         }
 
         // operator OverLoad
         _Iterator& operator=(_Iterator const &src)
         {
-            _ptr = src._ptr;
-            return (*this);
-        }
-        _Iterator& operator=(_Iterator const &src)
-        {
-            _ptr = src._ptr;
+            _ptrBase = src._ptrBase;
             return (*this);
         }
 
+        //pre
         _Iterator& operator++(void)
         {
-            this->_ptr++;
+            this->_ptrBase++;
             return (*this);
         }
+        _Iterator& operator--(void)
+        {
+            this->_ptrBase--;
+            return (*this);
+        }
+        //post
         _Iterator& operator++(int)
         {
             _Iterator tmp(*this);
-            this->src._ptr; 
+            this->_ptrBase++; 
             return tmp;
         }
+        _Iterator& operator--(int)
+        {
+            _Iterator tmp(*this);
+            this->_ptrBase--; 
+            return tmp;
+        }
+        
+        //Comparisions Operators : “==”, “!=”, “>”, “<”, “>=”,  “<=”
+        
+        bool operator==(_Iterator const *src)
+        {
+            return ((this->_ptrBase) == src->_ptrBase);
+        }
+        bool operator!=(_Iterator const *src)
+        {
+            return ((this->_ptrBase) != src->_ptrBase);
+        }
+        bool operator>(_Iterator const *src)
+        {
+            return ((this->_ptrBase) > src->_ptrBase);
+        }
+        bool operator <(_Iterator const *src)
+        {
+            return ((this->_ptrBase) < src->_ptrBase);
+        }
+        bool operator <=(_Iterator const *src)
+        {
+            return ((this->_ptrBase) <= src->_ptrBase);
+        }
+        bool operator >=(_Iterator const *src)
+        {
+            return ((this->_ptrBase) >= src->_ptrBase);
+        }
 
-    protected:
-        pointer _ptr;
+        //arithmetic operators (+, +=, -=, -)
+        _Iterator operator+(const _Iterator& s1, const _Iterator& s2)
+        {
+            return (s1._ptrBase + s2._ptrBase);
+        }
+        _Iterator operator+=(const _Iterator& s1, const _Iterator& s2)
+        {
+            return (s1._ptrBase + s2._ptrBase);
+        }
+        _Iterator operator-=(const _Iterator& s1, const _Iterator& s2)
+        {
+            return (s1._ptrBase - s2._ptrBase);
+        }
+        _Iterator operator-(const _Iterator& s1, const _Iterator& s2)
+        {
+            return (s1._ptrBase - s2._ptrBase);
+        }
+
+    private:
+        pointer _ptrBase;
     };
+
+
 }
