f68160a958ee1a51d5d0d19157bf7cfb716a105e
Ayoub <contact@maatouch.com>
2022-03-26 00:23:39 +0100

    adding new changes

--- Vector.hpp
+++ Vector.hpp
@@ -32,10 +32,7 @@ namespace ft
 
 		// construct/copy/destroy:
 		public:
-		explicit vector(const Allocator& alloc = Allocator()): _buffer(), _size(), _capacity()
-		{
-			_allocater = alloc;
-		}
+		explicit vector(const Allocator& alloc = Allocator()): _buffer(), _size(), _capacity(). _allocater(alloc) {}
 		explicit vector(size_type n, const T& value = T(),
             const Allocator& alloc = Allocator()): _allocater(alloc), _size(n), _capacity(n)
 			{
@@ -43,63 +40,118 @@ namespace ft
 				for (size_type i = 0; i < n ; i++)
 					_allocater.construct((_buffer + i), value);
 			}
-        template <class InputIterator>
-          vector(InputIterator first, InputIterator last,
-            const Allocator& alloc = Allocator(), typename enable_if<!ft::is_integral<InputIterator>::value, bool>::type = false)
-			{
+        // template <class InputIterator>
+        //   vector(InputIterator first, InputIterator last,
+        //     const Allocator& alloc = Allocator(), typename enable_if<!ft::is_integral<InputIterator>::value, bool>::type = false)
+		// 	{
 
-			}
+		// 	}
 
 
 
 	
 
+       ~vector()
+	   {
+		   _allocater.deallocate(_buffer, _capacity);
+	   }
 /***
         vector(const vector<T,Allocator>& x);
 
-       ~vector();
         vector<T,Allocator>& operator=(const vector<T,Allocator>& x);
         template <class InputIterator>
           void assign(InputIterator first, InputIterator last);
         void assign(size_type n, const T& u);
-        allocator_type get_allocator() const;
-	***//
+	***/
+        allocator_type get_allocator() const { return _allocater; }
 
 
 		// iterators:
 		iterator begin(){ return iterator(_buffer); }
 		const_iterator begin() const { return iterator(_buffer); }
 		iterator end() { return iterator((_buffer  + (_size ))); }
-		const_iterator end() const (){ return iterator((_buffer  + (_size ))); }
-		reverse_iterator rbegin() { return reverse_iterator( end() )} 
-		const_reverse_iterator rbegin() const { return reverse_iterator( end() )}
-		reverse_iterator rend() { return reverse_iterator( begin())} 
-		const_reverse_iterator rend() const { return reverse_iterator( begin())} 
+		const_iterator end() const { return iterator((_buffer  + (_size ))); }
+		reverse_iterator rbegin() { return reverse_iterator( end() );} 
+		const_reverse_iterator rbegin() const { return reverse_iterator(end());}
+		reverse_iterator rend() { return reverse_iterator( begin());} 
+		const_reverse_iterator rend() const { return reverse_iterator( begin());} 
+	
 	/***
-
 		// capacity:
 		***/
 		size_type size() const { return _size;}
 		size_type max_size() const { return _allocater.max_size(); }
-		bool empty()const ( return static_cast<bool>(_size); )
+		bool empty()const { return static_cast<bool>(_size); }
+		void reserve(size_type n)
+		{
+			if (n > max_size())
+				throw std::length_error("allocator<T>::allocate(size_t n) 'n' exceeds maximum supported size");
+			if (n > _capacity)
+			{
+				pointer _temp;
+				_temp = _allocater.allocate(n);
+				for (size_type i = 0; i < _size ; i++)
+					_allocater.construct((_temp + i), *(begin() + i));
+				clear();
+				_allocater.deallocate(_buffer, _capacity);
+				_capacity = n;
+				_buffer = _temp;
+			}
+		}
+		size_type capacity() const { return _capacity; }
+		
+		void resize(size_type sz, T c = T())
+		{
+			if (sz > _capacity)
+			{
+				(_capacity * 2) <= sz ? reserve(_capacity * 2) : reserve(sz) ;
+				for (; _size < _capacity; _size++)
+					_allocator.construct(_buffer + _size, val);
+			}
+			else
+				{
+					if (sz > _size)
+					{
+						// here implement 
+					}
+
+				}
+		}
+
 /***	
-		void resize(size_type sz, T c = T()); size_type capacity() const;
-		void reserve(size_type n);
 		
 	***/
 		// element access:
 		reference operator[](size_type n) { return *(begin() + n); }
-		const_reference operator[](size_type n) { const return *(begin() + n); } 
-		const_reference at(size_type n) const; 
-		reference at(size_type n);
-		reference front() {return *begin();}
-		const_reference front() const {return *begin();}
-		reference back() {return *(end() - 1);}
-		const_reference back() const {return *(end() - 1);}
-		/***
+		const_reference operator[](size_type n) const {  return *(begin() + n); } 
+		const_reference at(size_type n) const
+		{
+			if (n > _size)
+				std::out_of_range("vector");
+			return *(begin() + n);
+		}
+		reference at(size_type n)
+		{
+			if (n > _size)
+				std::out_of_range("vector");
+			return *(begin() + n);
+		}
+		reference front() { return *begin(); }
+		const_reference front() const { return *begin(); }
+		reference back() { return *(end() - 1); }
+		const_reference back() const { return *(end() - 1); }
+		
+		
 		// modifiers:
-		void push_back(const T& x);
-		void pop_back();
+
+		void push_back(const value_type &val) {
+			 resize(_size + 1, val); 
+			 }
+		void pop_back() {
+			 --_size;
+			 _allocater.destroy(_buffer + _size); 
+			 }
+		/***
 		iterator insert(iterator position, const T& x);
 		void insert(iterator position, size_type n, const T& x); template <class InputIterator>
         void insert(iterator position,
@@ -107,7 +159,11 @@ namespace ft
         iterator erase(iterator position);
         iterator erase(iterator first, iterator last);
         void     swap(vector<T,Allocator>&);
-        void     clear();           
 		*/
+        void     clear()
+		{
+			while (_size--)
+				_allocater.destroy((_buffer + _size));
+		}           
 		};
 }
--- main.cpp
+++ main.cpp
@@ -1,46 +1,24 @@
-// #include "vector.hpp"
+#include "vector.hpp"
 // #include "iterators.hpp"
 #include <iostream>
 // #include <memory>
-// #include <vector>
+#include <vector>
 // using namespace std;
 #include "is_intergal.hpp"
 
-    int main() {
-    std::cout << std::boolalpha;
-    std::cout << "is_integral:" << std::endl;
-    std::cout << "char: " << (bool)is_integral<char>::value << std::endl;
-    std::cout << "int: " << (bool)is_integral<int>::value << std::endl;
-    std::cout << "float: " << is_integral<float>::value << std::endl;
- 
-    // allocator for integer values
-    // allocator<int> myAllocator;
-  
-    // allocate space for five ints
-    // ::ft::vector<int*> here(5, 100);
-    // std::vector<int> hego(10, 10);
-    // std::vector<int>::iterator it = hego.begin();
-    // std::vector<int>::iterator en = hego.end();
+    int main() 
+    {
+        std::vector<int> vec(5, 15);
+        // ft::vector<int> _vec(5, 15);
     
-    // for (; it != en; it++)
-    //     std::cout << &it <<" - "<<(*it) << std::endl;
-    
-    
-    // int* arr = myAllocator.allocate(5);
-  
-    // // construct arr[0] and arr[3]
-    // for (int i = 0 ; i < 4; i++)
-    //     myAllocator.construct(arr + i, 100);
+        std::cout << vec.capacity()<<std::endl;
+        vec.resize(14, 20);
+        
+        std::cout << vec.capacity() <<std::endl;
 
-  
-    // cout << arr[3] << endl;
-    // cout << arr[4] << endl;
-    // cout << arr[0] << endl;
-    // cout << arr[1] << endl;
-    // cout << arr[2] << endl;
-  
-    // deallocate space for five ints
-    // myAllocator.deallocate(arr, 5);
-  
-    return 0;
+        
+
+    
+
+   return 0;
 }
